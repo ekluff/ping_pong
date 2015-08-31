cfd69fc0cdfc8a614630d193f75713c9170c139a
Evan Clough <clough.evan@gmail.com>
2015-08-31 11:05:43 -0700

    Correct spacing and indentation in file ping_pong.rb.

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -2,24 +2,18 @@ class Fixnum
   define_method(:ping_pong) do
 
     array_num = (1..self).to_a
-
     array_num.map! do |number|
 
-      if number % 3 == 0 && number % 5 == 0
-        "PININININGGGG PONNNNNNNNNG!!!!?!"
-      elsif number % 3 == 0
-        "ping"
-      elsif number % 5 == 0
-        "pong"
-      else
-        number
-
-      end
-
-
+    if number % 3 == 0 && number % 5 == 0
+      "PININININGGGG PONNNNNNNNNG!!!!?!"
+    elsif number % 3 == 0
+      "ping"
+    elsif number % 5 == 0
+      "pong"
+    else
+      number
     end
 
-
+    end
   end
-
 end
